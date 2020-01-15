62e6d2636d5725d75365cb150f20bbf84db54054
leeminjung1 <vipminjung@gmail.com>
2020-01-15 23:04:46 +0900

    11

--- src/index.js
+++ src/index.js
@@ -4,7 +4,37 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+//const element = <h1> Hello, world!</h1>
+
+/*const name = "Minjung";
+const element = <h1>Hello, {name}!</h1>*/
+
+/*const name={
+  firstName: "Minjung",
+  lastName: "Lee",
+};
+const element = <h1>Hello, {name.firstName + " " + name.lastName}!</h1>*/
+
+const formatName = function (name) {
+  return name.firstName + " " + name.lastName;
+}
+
+const name={
+  firstName:"Minjung",
+  lastName: "Lee",
+};
+
+const element = <h1>Hello, {formatName(name)}!</h1>
+
+
+
+
+
+ReactDOM.render(
+  element,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
