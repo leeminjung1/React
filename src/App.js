0d01b7115a4b57e7c3b826fd974ee36d92979d3f
leeminjung1 <vipminjung@gmail.com>
2020-01-16 01:34:14 +0900

    22

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Comment from './Comment';
+
+const comment = [
+  {name: "민정", content: 'mycomment'},
+  {name: "MinJungLee", content: 'mycomment'},
+  {name: "민정", content: 'mycomment'},
+  {name: "TonyStark", content: 'mycomment'},
+];
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{padding: 16, backgroundColor: '#282c34'}}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +28,15 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+          {comment.map((comment,index)=>{
+            return(
+              <Comment
+                  name={comment.name}
+                  content={comment.content} />
+            )
+          })}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,17 +6,19 @@ import * as serviceWorker from './serviceWorker';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 
-//const element = <h1> Hello, world!</h1>
-
-/*const name = "Minjung";
+ //const element = <h1> Hello, world!</h1>
+/*
+const name = "Minjung";
 const element = <h1>Hello, {name}!</h1>*/
 
 /*const name={
   firstName: "Minjung",
   lastName: "Lee",
 };
-const element = <h1>Hello, {name.firstName + " " + name.lastName}!</h1>*/
+const element = <h1>Hello, {name.firstName + " " + name.lastName}!</h1>
+*/
 
+/*
 const formatName = function (name) {
   return name.firstName + " " + name.lastName;
 }
@@ -25,16 +27,42 @@ const name={
   firstName:"Minjung",
   lastName: "Lee",
 };
-
 const element = <h1>Hello, {formatName(name)}!</h1>
+*/
+
+/* clock
+function tick (){
+  const element = (
+    <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+*/
+
+/*
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>
+}
+
+function App(){
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Sara" />
+      <Welcome name="Sara" />
+    </div>
+  );
+}
+*/
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root'));
 
 
+//setInterval(tick,1000);
 
 
-
-ReactDOM.render(
-  element,
-  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
